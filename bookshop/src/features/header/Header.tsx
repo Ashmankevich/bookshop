ed52bae46aec0b96db57a67c4c3bb98f2d04c7b6
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2022-07-22 09:15:20 +0300

    add icons notification for nav

--- bookshop/src/features/header/Header.tsx
+++ bookshop/src/features/header/Header.tsx
@@ -1,6 +1,14 @@
 import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
-import { LogoIcon, SearchIcon, ShoppingIcon, UserIcon } from "../../assets";
+import {
+  LogoIcon,
+  NotificationIcon,
+  SearchIcon,
+  ShoppingIcon,
+  UserIcon,
+} from "../../assets";
+import { useAppSelector } from "../../store/hooks";
+import { getCartInfo } from "../../store/selectors";
 import { AppPages } from "../../types";
 import { ButtonPrimary } from "../../ui/button/button-primary/ButtonPrimary";
 import { Input } from "../../ui/input/Input";
@@ -8,6 +16,7 @@ import style from "./Header.module.css";
 
 export const Header: React.FC = () => {
   const { register, handleSubmit } = useForm();
+  const { cart } = useAppSelector(getCartInfo);
   const navigate = useNavigate();
 
   const onSubmit = (data: any) => {
@@ -34,6 +43,13 @@ export const Header: React.FC = () => {
       <nav className={style.nav}>
         <Link to={AppPages.SHOPPING_CART}>
           <ShoppingIcon></ShoppingIcon>
+          {cart.length !== 0 ? (
+            <div className={style.notificationContainer}>
+              <NotificationIcon className={style.pic}></NotificationIcon>
+            </div>
+          ) : (
+            <div></div>
+          )}
         </Link>
         <Link to={AppPages.ACCOUNT}>
           <UserIcon></UserIcon>
