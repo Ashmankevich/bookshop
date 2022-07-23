f56e40d46d63e4d60d3b773876dab736ed13f069
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2022-07-23 21:31:31 +0300

    fix detailed-book page

--- bookshop/src/ui/books/book-details/BookDetails.tsx
+++ bookshop/src/ui/books/book-details/BookDetails.tsx
@@ -3,6 +3,7 @@ import { ButtonPrimary } from "../../button/button-primary/ButtonPrimary";
 import { GetBookDetailsApi } from "../../../store/types";
 import { addCart } from "../../../store/slices/cartSlice";
 import { useAppDispatch } from "../../../store/hooks";
+import { Title } from "../../title/Title";
 
 type BookDetailsProps = {
   book: GetBookDetailsApi;
@@ -15,9 +16,10 @@ export const BookDetails: React.FC<BookDetailsProps> = ({ book }) => {
   };
   return (
     <>
+      <Title className={style.bookTitle}>{book.title}</Title>
       <div className={style.wrapperBook} key={book.isbn13}>
         <div className={style.containerPic}>
-          <img className={style.pic} src={book.image} alt={book.title} />
+          <img className={style.pic} src={book.image} alt="" />
         </div>
         <div className={style.containerBook}>
           <div className={style.containerBookPrice}>
@@ -30,10 +32,12 @@ export const BookDetails: React.FC<BookDetailsProps> = ({ book }) => {
             <div className={style.fieldValue}>{book.authors}</div>
             <p className={style.nameField}>Publisher</p>
             <div className={style.fieldValue}>{book.publisher}</div>
-            <p className={style.nameField}>Pages</p>
-            <div className={style.fieldValue}>{book.pages}</div>
             <p className={style.nameField}>Year</p>
             <div className={style.fieldValue}>{book.year}</div>
+            <p className={style.nameField}>Pages</p>
+            <div className={style.fieldValue}>{book.pages}</div>
+            <p className={style.nameField}>URL</p>
+            <div className={style.fieldValue}>{book.url}</div>
           </div>
           <div className={style.containerButton}>
             <ButtonPrimary
@@ -45,6 +49,10 @@ export const BookDetails: React.FC<BookDetailsProps> = ({ book }) => {
             </ButtonPrimary>
           </div>
         </div>
+      </div>
+      <div className={style.containerDesc}>
+        <h3 className={style.DescTitle}>Description</h3>
+        <p className={style.DescText}>{book.desc}</p>
       </div>
     </>
   );
