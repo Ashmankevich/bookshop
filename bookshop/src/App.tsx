3ddc5c99fdf79772ea7607c7382b2bd092b129ae
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2022-07-31 14:13:54 +0300

    fix routes

--- bookshop/src/App.tsx
+++ bookshop/src/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { AppContext } from "./AppContext";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Account } from "./pages/Account";
 import { AppPages } from "./routes";
-import { Home } from "./pages/Home";
 import { SignIn } from "./pages/SignIn";
 import { SignUp } from "./pages/SignUp";
 import { ShoppingCart } from "./pages/ShoppingCart";
 import { Search } from "./features/search/Search";
 import { DetailedBook } from "./pages/DetailedBook";
+import { Home } from "./pages/Home";
 
 function App() {
   const appRef = React.createRef<HTMLDivElement>();
@@ -18,6 +18,8 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route index element={<Home />} />
+            <Route index element={<Navigate to={AppPages.HOME} />} />
+            <Route path={AppPages.HOME} element={<Home />} />
             <Route path={AppPages.ACCOUNT} element={<Account />} />
             <Route path={AppPages.DETAILED_BOOK} element={<DetailedBook />} />
             <Route path={AppPages.SEARCH} element={<Search />} />
