4c69ec3a42511847d3ebec0c52dfde654374403a
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2022-08-23 16:59:25 +0300

    continue to develop theme

--- bookshop/src/ui/toggle/Toggle.tsx
+++ bookshop/src/ui/toggle/Toggle.tsx
@@ -14,19 +14,17 @@ export const Toggle: React.FC = () => {
           onChange={(event) => {
             const style = appRef?.current!.style!;
             if (event.target.checked) {
-              style.setProperty("--primary-text-color", "white");
               style.setProperty(
                 "--background-color",
                 "var(--ds-background-color)"
               );
               style.setProperty(
-                "--btn-background-color",
-                "var(--ds-btn-background-color)"
+                "--primary-title-color",
+                "var(--ds-primary-color)"
               );
             } else {
-              style.removeProperty("--primary-text-color");
               style.removeProperty("--background-color");
-              style.removeProperty("--btn-background-color");
+              style.removeProperty("--primary-title-color");
             }
           }}
           readOnly
